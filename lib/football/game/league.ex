9f9e0c99327790ce8b0c318931e725f02bb88861
Charlotte Lorelei Oliveira <11341349+mememori@users.noreply.github.com>
2018-05-06 16:33:31 -0300

    Document Game.League

--- lib/football/game/league.ex
+++ lib/football/game/league.ex
@@ -1,7 +1,20 @@
 defmodule Football.Game.League do
+  @moduledoc """
+  Represents a game league.
+
+  A league is a group of sports teams that compete against each other.
+
+  Each league has an unique universal code (`t:code/0`) that identifies it
+  through the public API.
+  """
+
   use Ecto.Schema
 
-  @type t :: %__MODULE__{}
+  @type code :: String.t()
+  @type t :: %__MODULE__{
+          code: code(),
+          name: String.t()
+        }
   @type changeset :: changeset(Ecto.Changeset.action())
   @type changeset(action) :: %Ecto.Changeset{data: %__MODULE__{}, action: action}
 
@@ -12,6 +25,14 @@ defmodule Football.Game.League do
   end
 
   @type create(map) :: changeset(:insert)
+  @doc """
+  Prepares a changeset to _create_ a new League.
+
+  ## Parameters
+    - `code` - The unique code for the league (used on the public API for
+    search).
+    - `name` - The full name of the League.
+  """
   def create(params) do
     %__MODULE__{}
     |> cast(params, [:code, :name])
@@ -20,5 +41,19 @@ defmodule Football.Game.League do
     |> update_change(:code, &String.downcase/1)
     |> unique_constraint(:code)
     |> Map.put(:action, :insert)
+  end
+
+  @spec update(t | changeset, map) :: changeset(:update)
+  @doc """
+  Prepares a changeset to _update_ `data`.
+
+  ## Parameters
+    - `name` - The full name of the League.
+  """
+  def update(data, params) do
+    data
+    |> cast(params, :name)
+    |> validate_required([:name])
+    |> Map.put(:action, :update)
   end
 end
